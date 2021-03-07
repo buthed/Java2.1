1cf00a964ddd002bcb5683b3d26d2a2bba055fea
Oleg <oleg@localhost>
2021-03-07 23:45:15 +0300

    First

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[]args) {
+        int acount = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = acount + transfer;
+        System.out.println(total);
+    }
+}
+
+
+
